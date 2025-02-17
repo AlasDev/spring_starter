c254bf8f62e700eb2e7df0d57b4ec946cd63fac8
saladavide003 <davide.sala@itsrizzoli.it>
2025-02-17 14:48:15 +0100

    implemented mapping from list of Corse to list of id in foreign key

--- src/main/java/it/objectmethod/spring_starter/mapper/mapstruct/ClienteMapstructMapper.java
+++ src/main/java/it/objectmethod/spring_starter/mapper/mapstruct/ClienteMapstructMapper.java
@@ -3,9 +3,12 @@ package it.objectmethod.spring_starter.mapper.mapstruct;
 import it.objectmethod.spring_starter.dto.ClienteDTO;
 import it.objectmethod.spring_starter.dto.PageDTO;
 import it.objectmethod.spring_starter.entity.Cliente;
+import it.objectmethod.spring_starter.entity.Corsa;
 import it.objectmethod.spring_starter.util.BasicMethodMapping;
 import org.mapstruct.Builder;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.springframework.data.domain.Page;
 
 @Mapper(componentModel = "spring",
@@ -13,9 +16,21 @@ import org.springframework.data.domain.Page;
         builder = @Builder(disableBuilder = true))
 public interface ClienteMapstructMapper extends BasicMethodMapping<ClienteDTO, Cliente> {
 
+    @Mapping(target = "corse", source = "corse", qualifiedByName = "toCorseId")
     ClienteDTO mapToDto(Cliente cliente);
 
+    @Mapping(target = "corse", source = "corse", qualifiedByName = "toCorse")
     Cliente mapToEntity(ClienteDTO clienteDTO);
 
     PageDTO<ClienteDTO> mapToPageDTO(Page<Cliente> page);
+
+    @Named("toCorseId")
+    default Long toCorseId(Corsa corsa) {
+        return corsa.getId();
+    }
+
+    @Named("toCorse")
+    default Corsa toCorseId(Long id) {
+        return Corsa.builder().id(id).build();
+    }
 }
