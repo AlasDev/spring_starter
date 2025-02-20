6507a9a4320121850feacfaccdbbfeb774f11c8a
saladavide003 <davide.sala@itsrizzoli.it>
2025-02-20 14:25:43 +0100

    expanded ErrorBody

--- src/main/java/it/objectmethod/spring_starter/exception/ErrorBody.java
+++ src/main/java/it/objectmethod/spring_starter/exception/ErrorBody.java
@@ -1,20 +1,28 @@
 package it.objectmethod.spring_starter.exception;
 
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 import java.sql.Timestamp;
 
+import java.util.List;
+
 @Getter
 public class ErrorBody {
-    private final String message;
     private final Timestamp timestamp;
+    private final Long status;
+    private final String message;
+    private final List<String> errors;
 
     /**
-     * Constructor of ErrorBody
-     * @param message a message that explains what went wrong
+     * Constructor for ErrorBody
+     * @param message error message
+     * @param errors list of errors
      */
-    public ErrorBody(String message) {
-        this.message = message;
+    public ErrorBody(String message, HttpStatus status, List<String> errors) {
         this.timestamp = new Timestamp(System.currentTimeMillis());
+        this.status = (long) status.value();
+        this.message = message;
+        this.errors = errors;
     }
 }
