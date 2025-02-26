01292e0c85eff279f5e3fa1d983c655798f625a7
saladavide003 <davide.sala@itsrizzoli.it>
2025-02-26 11:51:04 +0100

    added mapstructMapper for User

--- src/main/java/it/objectmethod/spring_starter/mapper/mapstruct/UtenteMapstructMapper.java
+++ src/main/java/it/objectmethod/spring_starter/mapper/mapstruct/UtenteMapstructMapper.java
@@ -2,9 +2,11 @@ package it.objectmethod.spring_starter.mapper.mapstruct;
 
 import it.objectmethod.spring_starter.dto.UtenteDTO;
 import it.objectmethod.spring_starter.entity.Utente;
+import org.mapstruct.Builder;
 import org.mapstruct.Mapper;
 
-@Mapper
+@Mapper(componentModel = "spring",
+        builder = @Builder(disableBuilder = true))
 public interface UtenteMapstructMapper {
 
     UtenteDTO mapToDto(Utente utente);
