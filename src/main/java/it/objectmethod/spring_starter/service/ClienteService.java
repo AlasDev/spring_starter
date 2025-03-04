a3cdccb85787a2e96e4e0cb9e09bb0f95054e2e8
AlasDev <saladavide003@gmail.com>
2025-03-04 11:22:00 +0100

    modified ClienteService to add utente

--- src/main/java/it/objectmethod/spring_starter/service/ClienteService.java
+++ src/main/java/it/objectmethod/spring_starter/service/ClienteService.java
@@ -3,10 +3,12 @@ package it.objectmethod.spring_starter.service;
 import it.objectmethod.spring_starter.dto.ClienteDTO;
 import it.objectmethod.spring_starter.dto.PageDTO;
 import it.objectmethod.spring_starter.entity.Cliente;
+import it.objectmethod.spring_starter.entity.Utente;
 import it.objectmethod.spring_starter.exception.exceptions.RequiredValueException;
 import it.objectmethod.spring_starter.filter.ClienteSearchParams;
 import it.objectmethod.spring_starter.mapper.mapstruct.ClienteMapstructMapper;
 import it.objectmethod.spring_starter.repository.ClienteRepository;
+import it.objectmethod.spring_starter.repository.UtenteRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -18,10 +20,12 @@ import java.util.NoSuchElementException;
 public class ClienteService {
     private final ClienteRepository clienteRepository;
     private final ClienteMapstructMapper clienteMapstructMapper;
+    private final UtenteRepository utenteRepository;
 
-    public ClienteService(ClienteRepository clienteRepository, ClienteMapstructMapper clienteMapstructMapper) {
+    public ClienteService(ClienteRepository clienteRepository, ClienteMapstructMapper clienteMapstructMapper, UtenteRepository utenteRepository) {
         this.clienteRepository = clienteRepository;
         this.clienteMapstructMapper = clienteMapstructMapper;
+        this.utenteRepository = utenteRepository;
     }
 
     public List<ClienteDTO> getAll() {
@@ -37,16 +41,23 @@ public class ClienteService {
 
     public ClienteDTO updateCliente(ClienteDTO clienteDTO) {
         Long clienteId = clienteDTO.getId(); //Id
+        Long utenteId = clienteDTO.getUtente(); //Utente
 
         if (clienteId == null) {
             throw new RequiredValueException("Id");
+        }
+        if (utenteId == null) {
+            throw new RequiredValueException("Utente");
         }
 
         clienteMapstructMapper.mapToDto(clienteRepository.findById(clienteId).orElseThrow(
                 () -> new NoSuchElementException("Cliente with id '" + clienteId + "' not found")
         ));
+        Utente utente = utenteRepository.findById(utenteId).orElseThrow(
+                () -> new NoSuchElementException("Utente with id '" + utenteId + "' not found"));
 
         Cliente cliente = clienteMapstructMapper.mapToEntity(clienteDTO);
+        cliente.setUtente(utente);
         Cliente clienteUpdated = clienteRepository.save(cliente);
         return clienteMapstructMapper.mapToDto(clienteUpdated);
     }
@@ -61,7 +72,17 @@ public class ClienteService {
     public ClienteDTO save( ClienteDTO clienteDTO) {
         clienteDTO.setId(null);
 
+        if (clienteDTO.getUtente() == null) {
+            throw new RequiredValueException("Utente");
+        }
+
+        Long utenteId = clienteDTO.getUtente();
+
+        Utente utente = utenteRepository.findById(utenteId).orElseThrow(
+                () -> new NoSuchElementException("Utente with id '" + utenteId + "' not found"));
+
         Cliente cliente = clienteMapstructMapper.mapToEntity(clienteDTO);
+        cliente.setUtente(utente);
         Cliente clienteSaved = clienteRepository.save(cliente);
         return clienteMapstructMapper.mapToDto(clienteSaved);
     }
