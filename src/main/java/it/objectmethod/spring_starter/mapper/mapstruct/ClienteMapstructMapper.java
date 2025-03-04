c5e68f0dedd08b2fc86d1c767294f38305bbf675
AlasDev <saladavide003@gmail.com>
2025-03-04 11:06:39 +0100

    modified Cliente's mapstruct to fix utente mapping

--- src/main/java/it/objectmethod/spring_starter/mapper/mapstruct/ClienteMapstructMapper.java
+++ src/main/java/it/objectmethod/spring_starter/mapper/mapstruct/ClienteMapstructMapper.java
@@ -4,6 +4,7 @@ import it.objectmethod.spring_starter.dto.ClienteDTO;
 import it.objectmethod.spring_starter.dto.PageDTO;
 import it.objectmethod.spring_starter.entity.Cliente;
 import it.objectmethod.spring_starter.entity.Corsa;
+import it.objectmethod.spring_starter.entity.Utente;
 import it.objectmethod.spring_starter.util.BasicMethodMapping;
 import org.mapstruct.Builder;
 import org.mapstruct.Mapper;
@@ -17,9 +18,11 @@ import org.springframework.data.domain.Page;
 public interface ClienteMapstructMapper extends BasicMethodMapping<ClienteDTO, Cliente> {
 
     @Mapping(target = "corse", source = "corse", qualifiedByName = "toCorseId")
+    @Mapping(target = "utente", source = "utente", qualifiedByName = "toUtenteId")
     ClienteDTO mapToDto(Cliente cliente);
 
     @Mapping(target = "corse", source = "corse", qualifiedByName = "toCorse")
+    @Mapping(target = "utente", source = "utente", qualifiedByName = "toUtente")
     Cliente mapToEntity(ClienteDTO clienteDTO);
 
     PageDTO<ClienteDTO> mapToPageDTO(Page<Cliente> page);
@@ -32,5 +35,15 @@ public interface ClienteMapstructMapper extends BasicMethodMapping<ClienteDTO, C
     @Named("toCorse")
     default Corsa toCorseId(Long id) {
         return Corsa.builder().id(id).build();
+    }
+
+    @Named("toUtenteId")
+    default Long toUtenteId(Utente utente) {
+        return utente.getId();
+    }
+
+    @Named("toUtente")
+    default Utente toUtente(Long id) {
+        return Utente.builder().id(id).build();
     }
 }
