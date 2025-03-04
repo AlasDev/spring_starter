821b04d478b4dd22edaa823b24e590e2a83ae45b
AlasDev <saladavide003@gmail.com>
2025-03-04 11:02:06 +0100

    added repository for Ruolo, StatoCorsa, Utente

---
+++ src/main/java/it/objectmethod/spring_starter/repository/RuoloRepository.java
@@ -0,0 +1,7 @@
+package it.objectmethod.spring_starter.repository;
+
+import it.objectmethod.spring_starter.entity.Ruolo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface RuoloRepository extends JpaRepository<Ruolo, Long>, JpaSpecificationExecutor<Ruolo> {}
---
+++ src/main/java/it/objectmethod/spring_starter/repository/StatoCorsaRepository.java
@@ -0,0 +1,7 @@
+package it.objectmethod.spring_starter.repository;
+
+import it.objectmethod.spring_starter.entity.StatoCorsa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface StatoCorsaRepository extends JpaRepository<StatoCorsa, Long>, JpaSpecificationExecutor<StatoCorsa> {}
