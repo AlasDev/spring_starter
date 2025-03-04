d539ea2be38e368785db9f55d8ab7e5689c2ef0e
AlasDev <saladavide003@gmail.com>
2025-03-04 14:14:46 +0100

    added auth "Cross-Origin Resource Sharing"

---
+++ src/main/java/it/objectmethod/spring_starter/authentication/CorsFilter.java
@@ -0,0 +1,36 @@
+package it.objectmethod.spring_starter.authentication;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@Order(0)
+public class CorsFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+//        response.setContentType();
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+
+        /*
+         * Used by front-ent to make sure that backend is working (operation of pre-flight)
+         */
+        if (request.getMethod().equalsIgnoreCase("OPTIONS")) {
+            response.setStatus(HttpServletResponse.SC_OK);
+            return;
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
