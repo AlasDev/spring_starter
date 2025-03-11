00e806d02944ecd9ff0b54ddcaa35f7097bd01bf
AlasDev <saladavide003@gmail.com>
2025-03-11 16:08:15 +0100

    deleted Ruolo table

--- src/main/java/it/objectmethod/spring_starter/repository/RuoloRepository.java
+++
@@ -1,8 +0,0 @@
-package it.objectmethod.spring_starter.repository;
-
-import it.objectmethod.spring_starter.entity.Ruolo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface RuoloRepository extends JpaRepository<Ruolo, Long>, JpaSpecificationExecutor<Ruolo> {
-}
