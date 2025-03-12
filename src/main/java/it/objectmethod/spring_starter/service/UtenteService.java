62394bb950b7e78cfa72a1e7340694de09a70594
AlasDev <saladavide003@gmail.com>
2025-03-12 10:02:56 +0100

    removed Ruolo table with its enum and replaced it by using Role enum directly into Utente

--- src/main/java/it/objectmethod/spring_starter/service/UtenteService.java
+++ src/main/java/it/objectmethod/spring_starter/service/UtenteService.java
@@ -2,13 +2,12 @@ package it.objectmethod.spring_starter.service;
 
 import it.objectmethod.spring_starter.dto.PageDTO;
 import it.objectmethod.spring_starter.dto.UtenteDTO;
-import it.objectmethod.spring_starter.entity.Ruolo;
 import it.objectmethod.spring_starter.entity.Utente;
 import it.objectmethod.spring_starter.exception.exceptions.RequiredValueException;
 import it.objectmethod.spring_starter.filter.UtenteSearchParams;
-import it.objectmethod.spring_starter.mapper.mapstruct.UtenteMapstructMapper;
-import it.objectmethod.spring_starter.repository.RuoloRepository;
+import it.objectmethod.spring_starter.mapper.UtenteMapstructMapper;
 import it.objectmethod.spring_starter.repository.UtenteRepository;
+import it.objectmethod.spring_starter.util.Role;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -20,12 +19,10 @@ import java.util.NoSuchElementException;
 public class UtenteService {
     private final UtenteRepository utenteRepository;
     private final UtenteMapstructMapper utenteMapstructMapper;
-    private final RuoloRepository ruoloRepository;
 
-    public UtenteService(UtenteRepository utenteRepository, UtenteMapstructMapper utenteMapstructMapper, RuoloRepository ruoloRepository) {
+    public UtenteService(UtenteRepository utenteRepository, UtenteMapstructMapper utenteMapstructMapper) {
         this.utenteRepository = utenteRepository;
         this.utenteMapstructMapper = utenteMapstructMapper;
-        this.ruoloRepository = ruoloRepository;
     }
 
     public List<UtenteDTO> getAll() {
@@ -41,18 +38,17 @@ public class UtenteService {
 
     public UtenteDTO updateUtente(UtenteDTO utenteDTO) {
         Long utenteId = utenteDTO.getId(); //Id
-        Long ruoloId = utenteDTO.getRuolo();
+        Role ruolo = utenteDTO.getRuolo();
+
         if (utenteId == null) {
             throw new RequiredValueException("Id");
         }
-        if (ruoloId == null) {
+        if (ruolo == null) {
             throw new RequiredValueException("Ruolo");
         }
 
         utenteMapstructMapper.mapToDto(utenteRepository.findById(utenteId).orElseThrow(
                 () -> new NoSuchElementException("Utente with id '" + utenteId + "' not found")));
-        Ruolo ruolo = ruoloRepository.findById(utenteId).orElseThrow(
-                () -> new NoSuchElementException("Ruolo with id '" + ruoloId + "' not found"));
 
         Utente utente = utenteMapstructMapper.mapToEntity(utenteDTO);
         utente.setRuolo(ruolo);
@@ -69,17 +65,14 @@ public class UtenteService {
 
     public UtenteDTO save(UtenteDTO utenteDTO) {
         utenteDTO.setId(null);
-        Long ruoloId = utenteDTO.getRuolo();
+        Role role = utenteDTO.getRuolo();
 
-        if (ruoloId == null) {
+        if (role == null) {
             throw new RequiredValueException("Ruolo");
         }
 
-        Ruolo ruolo = ruoloRepository.findById(ruoloId).orElseThrow(
-                () -> new NoSuchElementException("Ruolo with id '" + ruoloId + "' not found"));
-
         Utente utente = utenteMapstructMapper.mapToEntity(utenteDTO);
-        utente.setRuolo(ruolo);
+        utente.setRuolo(role);
         utenteRepository.save(utente);
         return utenteMapstructMapper.mapToDto(utente);
     }
