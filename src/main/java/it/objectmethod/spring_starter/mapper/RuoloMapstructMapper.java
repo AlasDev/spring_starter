aada1d4b581dc89c7fe8fa16e40d3db644e26469
AlasDev <saladavide003@gmail.com>
2025-03-19 10:27:02 +0100

    added mapping for utentiDTO-->utenteRuoli utenteRuoli-->utentiDTO conversions

--- src/main/java/it/objectmethod/spring_starter/mapper/RuoloMapstructMapper.java
+++ src/main/java/it/objectmethod/spring_starter/mapper/RuoloMapstructMapper.java
@@ -1,17 +1,54 @@
 package it.objectmethod.spring_starter.mapper;
 
 import it.objectmethod.spring_starter.dto.RuoloDTO;
+import it.objectmethod.spring_starter.dto.UtenteDTO;
 import it.objectmethod.spring_starter.entity.Ruolo;
+import it.objectmethod.spring_starter.entity.Utente;
+import it.objectmethod.spring_starter.entity.UtenteRuolo;
 import it.objectmethod.spring_starter.util.BasicMethodMapping;
 import org.mapstruct.Builder;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+import org.mapstruct.factory.Mappers;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Mapper(componentModel = "spring",
         uses = {UtenteMapstructMapper.class},
         builder = @Builder(disableBuilder = true))
 public interface RuoloMapstructMapper extends BasicMethodMapping<RuoloDTO, Ruolo> {
 
+    UtenteMapstructMapper utenteMapper = Mappers.getMapper(UtenteMapstructMapper.class);
+
+    @Mapping(target = "utenti", source = "utenteRuoli", qualifiedByName = "toUtenti")
     RuoloDTO mapToDto(Ruolo ruolo);
 
+    @Mapping(target = "utenteRuoli", source = "utenti", qualifiedByName = "toUtenteRuoli")
     Ruolo mapToEntity(RuoloDTO ruoloDTO);
+
+    @Named("toUtenti")
+    default List<UtenteDTO> toUtenteDto(List<UtenteRuolo> utenteRuoli) {
+        List<UtenteDTO> utenteDTOS = new ArrayList<>();
+        for (UtenteRuolo utenteRuolo : utenteRuoli) {
+            if (utenteRuolo != null) {
+                UtenteDTO utenteDTO = utenteMapper.mapToDto(utenteRuolo.getUtente());
+                utenteDTOS.add(utenteDTO);
+            }
+        }
+        return utenteDTOS;
+    }
+
+    @Named("toUtenteRuoli")
+    default List<UtenteRuolo> toUtenteRuoli(List<UtenteDTO> utenteDTOS) {
+        List<UtenteRuolo> utenteRuoli = new ArrayList<>();
+        for (UtenteDTO utenteDTO : utenteDTOS) {
+            Utente utente = utenteMapper.mapToEntity(utenteDTO);
+            if (utenteDTO != null) {
+                utenteRuoli.add(new UtenteRuolo(utente, null));  // Il ruolo verrà settato successivamente
+            }
+        }
+        return utenteRuoli;
+    }
 }
