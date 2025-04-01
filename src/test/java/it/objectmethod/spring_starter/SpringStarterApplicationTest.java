bfbd77cf05682d60753095c19af0cb9fe5f3602a
AlasDev <saladavide003@gmail.com>
2025-04-01 10:30:14 +0200

    added files to perform Integration tests

---
+++ src/test/java/it/objectmethod/spring_starter/SpringStarterApplicationTest.java
@@ -0,0 +1,11 @@
+package it.objectmethod.spring_starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringStarterApplicationTest {
+
+    @Test
+    void contextLoads() {}
+}
